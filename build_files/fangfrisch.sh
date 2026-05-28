7acec9b8a36ec38c976eb786ee0e6c9c2559629e
Analetta "Annie" Ehler <annie.ehler.4@gmail.com>
2026-05-27 23:03:46 -0700

    Ensure the local user directories exist
    Signed-off-by: Analetta "Annie" Ehler <annie.ehler.4@gmail.com>

--- build_files/fangfrisch.sh
+++ build_files/fangfrisch.sh
@@ -3,6 +3,10 @@ set -Eeuxo pipefail
 
 { export PS4='+( ${BASH_SOURCE}:${LINENO} ): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'; } 2>/dev/null
 
+if ! [ -d /usr/local/share ]; then
+  mkdir -p /usr/local/share
+fi
+
 python3 -m venv /usr/local/share/fangfrisch-venv
 # shellcheck disable=SC1091
 source /usr/local/share/fangfrisch-venv/bin/activate
